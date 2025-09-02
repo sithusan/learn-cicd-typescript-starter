83aeedf9f7849bdea52abc4ce3b47388f3a471cf
Si Thu San <sithusanss@gmail.com>
2025-09-02 18:04:26 +0700

    test: add test
    * docs: add name
    
    * build: ci
    
    * build: check node version
    
    * test: add test for auth
    
    * build: run tests in ci
    
    * build: intentionally make it fail
    
    * build: fix failed
    
    * build: fix dependencies
    
    * build: fix dependencies
    
    * build: coverage
    
    * build: enhance the cmd
    
    * build: readme badge

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,47 @@
+import { IncomingHttpHeaders } from "http";
+import { getAPIKey } from "src/api/auth";
+import { describe, expect, test } from "vitest";
+
+describe("get API Key", () => {
+  const apiKey = "asdf1234";
+
+  test("can get API key", () => {
+    const header: IncomingHttpHeaders = {
+      authorization: `ApiKey ${apiKey}`,
+    };
+
+    const result = getAPIKey(header);
+
+    expect(result).toBe(apiKey);
+  });
+
+  test("API key null without authorization header", () => {
+    const header: IncomingHttpHeaders = {
+      //   authorization: `ApiKey ${apiKey}`,
+    };
+
+    const result = getAPIKey(header);
+
+    expect(result).toBeNull();
+  });
+
+  test("API key null on no values after key", () => {
+    const header: IncomingHttpHeaders = {
+      authorization: `ApiKey`,
+    };
+
+    const result = getAPIKey(header);
+
+    expect(result).toBeNull();
+  });
+
+  test("API key null on key mismatch", () => {
+    const header: IncomingHttpHeaders = {
+      authorization: `ApiKeys ${apiKey}`,
+    };
+
+    const result = getAPIKey(header);
+
+    expect(result).toBeNull();
+  });
+});
